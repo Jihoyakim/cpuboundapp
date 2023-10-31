5e49d7230428ed775fd17336930e4853fd1e6383
Jihoyakim <kjh970201@gmail.com>
2023-10-31 12:32:30 +0900

    rara

--- src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd test docker docker";
+        return "hello ci/cd test rameon";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
