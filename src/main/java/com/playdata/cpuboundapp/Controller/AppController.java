488bccc4b693ee2d42414d817ce966a132d9a336
Jihoyakim <kjh970201@gmail.com>
2023-10-31 12:48:51 +0900

    asdsf

--- src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd test rasd";
+        return "hello ci/cd test bbc";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
