69a1bd71a969879258fec1e3c9bd42ab73440b39
Jihoyakim <kjh970201@gmail.com>
2023-11-01 11:26:45 +0900

    baefo

--- src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/Controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello slepp ";
+        return "hello new baefo ";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
